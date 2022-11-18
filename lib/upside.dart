d889c4fd53bbeb50cf6cc38cb572f50527fe583e
Nurdauletski <gethipe1@mail.ru>
2022-11-18 20:52:27 +0600

    deleted some files

--- lib/upside.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class Upside extends StatelessWidget {
-  const Upside({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
